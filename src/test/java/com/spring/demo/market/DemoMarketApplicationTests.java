528a7e29c5f81b2b2f8b19b1264001df5e380772
alvelur05 <alejandrovelasquezuribe@gmail.com>
2023-12-27 13:14:39 -0500

    Setting Spring app

---
+++ src/test/java/com/spring/demo/market/DemoMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.demo.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
