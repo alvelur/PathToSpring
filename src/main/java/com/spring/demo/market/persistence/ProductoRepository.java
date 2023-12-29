5fba9f0fb761dc390ec07beccb47ebd4815e433a
alvelur05 <alejandrovelasquezuribe@gmail.com>
2023-12-28 23:17:54 -0500

    UsandoMapStruc para mapear mis clases

--- src/main/java/com/spring/demo/market/persistence/ProductoRepository.java
+++ src/main/java/com/spring/demo/market/persistence/ProductoRepository.java
@@ -2,10 +2,12 @@ package com.spring.demo.market.persistence;
 
 import com.spring.demo.market.persistence.crud.ProductoCrudRepository;
 import com.spring.demo.market.persistence.entity.Producto;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public class ProductoRepository {
     private ProductoCrudRepository productoCrudRepository;
     public List<Producto> getAll(){
@@ -17,5 +19,17 @@ public class ProductoRepository {
 
     public Optional<List<Producto>> getEscasos(int cantidad){
         return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad, true);
+    }
+
+    public Optional<Producto> getProduct(int idProducto){
+        return productoCrudRepository.findById(idProducto);
+    }
+
+    public Producto save(Producto producto){
+        return productoCrudRepository.save(producto);
+    }
+
+    public void delete(int idProducto){
+        productoCrudRepository.deleteById(idProducto);
     }
 }
